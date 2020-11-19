57798b2b7af3576a5ae6202e56807095672a939d
qpWei <42760426+qipengwei@users.noreply.github.com>
2020-11-20 00:09:19 +0800

    天数输出 (#76)
    天数输出 (#76)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -20,6 +20,53 @@ public class Main {
      * @return 结果字符串
      */
     public static String howManyDaysInMonth(int month) {
-        return "";
+
+        int days = 0;
+        switch (month){
+            case 1:
+                days=31;
+                break;
+            case 2:
+                days=28;
+                break;
+            case 3:
+                days=31;
+                break;
+            case 4:
+                days=30;
+                break;
+            case 5:
+                days=31;
+                break;
+            case 6:
+                days=30;
+                break;
+            case 7:
+                days=31;
+                break;
+            case 8:
+                days=31;
+                break;
+            case 9:
+                days=30;
+                break;
+            case 10:
+                days=31;
+                break;
+            case 11:
+                days=30;
+                break;
+            case 12:
+                days=31;
+                break;
+            default:
+                days=0;
+                break;
+        }
+        if(days==0) {
+            return "非法输入";
+        }else{
+            return month+"月有"+days+"天";
+        }
     }
 }
