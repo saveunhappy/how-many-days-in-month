e82698a743431e5cd64ea334fb9fe55d4a3f3b5d
soohea <77014206+soohea@users.noreply.github.com>
2021-09-08 14:58:17 +0800

    how-many-days-in-month (#97)
    how-many-days-in-month (#97)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.lang.invoke.SwitchPoint;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(howManyDaysInMonth(0));
@@ -20,6 +22,24 @@ public class Main {
      * @return 结果字符串
      */
     public static String howManyDaysInMonth(int month) {
-        return "";
+        switch(month){
+            case(1):
+            case(3):
+            case(5):
+            case(7):
+            case(8):
+            case(10):
+            case(12):
+                return(month + "月有31天");
+            case(2):
+                return(month + "月有28天");
+            case(4):
+            case(6):
+            case(9):
+            case(11):
+                return (month + "月有30天");
+            default:
+                return"非法输入";
+        }
     }
 }
